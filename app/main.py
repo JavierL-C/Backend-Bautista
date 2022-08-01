97b6d60c2d96b229e88dfc082bab890c0271f321
RochaMoran <rochamoran1@gmail.com>
2022-08-01 12:24:35 -0600

    ADD: configuration CORS in the project

--- app/main.py
+++ app/main.py
@@ -2,10 +2,24 @@ from fastapi import FastAPI
 from app.models import user
 from app.db.config import engine
 from app.routers import user_routers
+from fastapi.middleware.cors import CORSMiddleware
 
 user.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 async def Home():
