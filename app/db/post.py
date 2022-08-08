a9f6f1c5c4b510b87f9ea2760ebdf799da08b844
JavierL-C <javierlumbi23@gmail.com>
2022-08-08 01:43:46 -0600

    holder for post

---
+++ app/db/post.py
@@ -0,0 +1,42 @@
+from pickle import FALSE
+from sqlalchemy.orm import Session
+from datetime import datetime
+from app.models.user import Post
+from app.schemas.post_schemas import RequestPost
+
+def get_post(db:Session, skip:int=0, limit:int=1):
+    return db.query(Post).offset(skip).limit(limit).all()
+
+def create_post(db:Session, post:RequestPost):
+    _post = Post(
+        post_content = post.parameter.post_content,
+        post_is_approved = False,
+        post_create_date = datetime.now(),
+        post_update_date = datetime.now(),
+        post_user_id = post.parameter.post_user_id,
+        post_image = post.parameter.post_image,
+    )
+    db.add(_post)
+    db.commit()
+    db.refresh(_post)
+    return _post
+
+
+def get_post_by_id(db:Session, post_id:int):
+    return db.query(Post).filter(Post.post_id == post_id).first()
+
+def update_post(db:Session, post:RequestPost, post_id:int):
+    _post = get_post_by_id(db=db, post_id=post_id)
+    _post.post_content = post.parameter.post_content
+    _post.post_is_approved = post.parameter.post_is_approved
+    _post.post_user_id = post.parameter.post_user_id
+    _post.post_image = post.parameter.post_image
+    _post.post_update_date = datetime.now()
+    db.commit()
+    db.refresh(_post)
+    return _post
+
+def remove_post(db:Session, post_id:int):
+    _post = get_post_by_id(db=db, post_id=post_id)
+    db.delete(_post)
+    db.commit()
