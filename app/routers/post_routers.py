0a98e2842e36538d98774f44a354b428d4e7d418
RochaMoran <rochamoran1@gmail.com>
2022-08-14 21:30:08 -0600

    FIX: error image the schema post and routers

--- app/routers/post_routers.py
+++ app/routers/post_routers.py
@@ -9,6 +9,7 @@ from app.schemas.post_schemas import PostCommentSchema, PostResponse, PostSchema
 
 router = APIRouter()
 
+
 def get_db():
     db = SessionLocal()
     try:
@@ -16,8 +17,9 @@ def get_db():
     finally:
         db.close()
 
+
 @router.get("/")
-async def get(db:Session=Depends(get_db)):
+async def get(db: Session = Depends(get_db)):
     try:
         _posts = post.get_post(db, 0, 100)
         for _post in _posts:
@@ -30,13 +32,14 @@ async def get(db:Session=Depends(get_db)):
         )
     except:
         return PostResponse(
-        code=404,
-        status="fail",
-        message="Something was wrong"
-    )
+            code=404,
+            status="fail",
+            message="Something was wrong"
+        )
+
 
 @router.get("/andcomments/{post_id}")
-async def get(post_id, db:Session=Depends(get_db)):
+async def get(post_id, db: Session = Depends(get_db)):
     try:
         _post = post.get_post_by_id(db, post_id=post_id)
         if _post == None:
@@ -45,36 +48,36 @@ async def get(post_id, db:Session=Depends(get_db)):
                 status="ok",
                 message="Post not found",
             )
-        
+
         _comment = comment.get_comment_by_post(db, _post.__dict__['post_id'])
         _theResult = PostCommentSchema(
             post_content=_post.__dict__['post_content'],
             post_title=_post.__dict__['post_title'],
-            post_image=_post.__dict__['post_image'],
             post_is_approved=_post.__dict__['post_is_approved'],
             post_user_id=_post.__dict__['post_user_id'],
             post_create_date=_post.__dict__['post_create_date'],
             post_update_date=_post.__dict__['post_update_date'],
             post_comments=_comment
         )
-        
+
         return PostResponse(
             code=200,
             status="ok",
             message="Success fetch all data",
-            result= _theResult
+            result=_theResult
         )
     except:
-         return PostResponse(
+        return PostResponse(
             code=401,
             status="fail",
             message="Something was wrong"
         )
 
+
 @router.get("/andcomments/{post_id}/{is_approved}")
-async def get(post_id, is_approved, db:Session=Depends(get_db)):
+async def get(post_id, is_approved, db: Session = Depends(get_db)):
     try:
-        
+
         _is_approved = True if is_approved == "true" else False
         _post = post.get_post_by_id(db, post_id=post_id)
         if _post == None:
@@ -83,33 +86,34 @@ async def get(post_id, is_approved, db:Session=Depends(get_db)):
                 status="ok",
                 message="Post not found",
             )
-        _comment = comment.get_comment_by_post_and_comment(db, _post.__dict__['post_id'], _is_approved)
+        _comment = comment.get_comment_by_post_and_comment(
+            db, _post.__dict__['post_id'], _is_approved)
         _theResult = PostCommentSchema(
             post_content=_post.__dict__['post_content'],
             post_title=_post.__dict__['post_title'],
-            post_image=_post.__dict__['post_image'],
             post_is_approved=_post.__dict__['post_is_approved'],
             post_user_id=_post.__dict__['post_user_id'],
             post_create_date=_post.__dict__['post_create_date'],
             post_update_date=_post.__dict__['post_update_date'],
             post_comments=_comment
         )
-        
+
         return PostResponse(
             code=200,
             status="ok",
             message="Success fetch all data",
-            result= _theResult
+            result=_theResult
         )
     except:
-         return PostResponse(
+        return PostResponse(
             code=401,
             status="fail",
             message="Something was wrong"
         )
 
+
 @router.get("/{post_id}")
-async def get_post(post_id, db:Session=Depends(get_db)):
+async def get_post(post_id, db: Session = Depends(get_db)):
     try:
         intId = int(post_id)
         _post = post.get_post_by_id(db, intId)
@@ -128,8 +132,9 @@ async def get_post(post_id, db:Session=Depends(get_db)):
             message="Invalid token"
         )
 
+
 @router.post('/update/{post_id}')
-async def update_post(post_id, image:UploadFile, request:PostSchema=Depends(PostSchema.as_form), db:Session=Depends(get_db)):
+async def update_post(post_id, image: UploadFile, request: PostSchema = Depends(PostSchema.as_form), db: Session = Depends(get_db)):
     try:
         intId = int(post_id)
 
@@ -163,8 +168,9 @@ async def update_post(post_id, image:UploadFile, request:PostSchema=Depends(Post
             message="Invalid token"
         )
 
+
 @router.delete('/delete/{post_id}')
-async def delete_post(post_id, db:Session=Depends(get_db)):
+async def delete_post(post_id, db: Session = Depends(get_db)):
     try:
         intId = int(post_id)
         exist_post = post.get_post_by_id(db, intId)
@@ -188,8 +194,9 @@ async def delete_post(post_id, db:Session=Depends(get_db)):
             message="Invalid token"
         )
 
+
 @router.post('/create')
-async def create(image:UploadFile, request:PostSchema=Depends(PostSchema.as_form), db:Session=Depends(get_db)):
+async def create(image: UploadFile, request: PostSchema = Depends(PostSchema.as_form), db: Session = Depends(get_db)):
     _new_post = post.create_post(db, request, image.file)
     if _new_post is None:
         return PostResponse(
@@ -205,6 +212,7 @@ async def create(image:UploadFile, request:PostSchema=Depends(PostSchema.as_form
         # result=_new_post
     ).dict(exclude_none=True)
 
+
 @router.get('/image/{post_id}')
-async def get_post_image_by_id(post_id, db:Session=Depends(get_db)):
+async def get_post_image_by_id(post_id, db: Session = Depends(get_db)):
     return FileResponse('Hongo.jpg')
--- app/schemas/post_schemas.py
+++ app/schemas/post_schemas.py
@@ -5,18 +5,20 @@ from pydantic.generics import GenericModel
 
 T = TypeVar("T")
 
+
 class PostSchema(BaseModel):
-    post_content: Optional[str]=None
-    post_title: Optional[str]=None
-    post_is_approved: Optional[bool]=False
-    post_user_id: Optional[int]=Field(default=None, foreign_key="users.user_id")
-    post_content_html: Optional[str]=None
+    post_content: Optional[str] = None
+    post_title: Optional[str] = None
+    post_is_approved: Optional[bool] = False
+    post_user_id: Optional[int] = Field(
+        default=None, foreign_key="users.user_id")
+    post_content_html: Optional[str] = None
 
     class Config:
         orm_mode = True
 
     @classmethod
-    def as_form(cls, post_content:str, post_title:str, post_is_approved:bool, post_user_id:int, post_content_html:str) -> 'PostSchema':
+    def as_form(cls, post_content: str, post_title: str, post_is_approved: bool, post_user_id: int, post_content_html: str) -> 'PostSchema':
         return cls(
             post_content=post_content,
             post_title=post_title,
@@ -25,21 +27,25 @@ class PostSchema(BaseModel):
             post_content_html=post_content_html
         )
 
+
 class PostCommentSchema(BaseModel):
-    post_content: Optional[str]=None
-    post_title: Optional[str]=None
-    post_is_approved: Optional[bool]=False
-    post_user_id: Optional[int]=Field(default=None, foreign_key="users.user_id")
-    post_create_date: Optional[date]=None
-    post_update_date: Optional[date]=None
-    post_image: Optional[str]=None
-    post_comments: Union[list, None]=None
+    post_content: Optional[str] = None
+    post_title: Optional[str] = None
+    post_is_approved: Optional[bool] = False
+    post_user_id: Optional[int] = Field(
+        default=None, foreign_key="users.user_id")
+    post_create_date: Optional[date] = None
+    post_update_date: Optional[date] = None
+    post_comments: Union[list, None] = None
+
 
 class RequestPost(BaseModel):
     parameter: PostSchema = Field(...)
 
+
 class RequestPostComment(BaseModel):
     parameter: PostCommentSchema = Field(...)
+
 
 class PostResponse(GenericModel, Generic[T]):
     code: str
